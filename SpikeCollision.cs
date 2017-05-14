b696e3b8ce75f7ea9c0e56c3121273740c59bb40
shebkoch <shebkoch@gmail.com>
2017-05-14 22:55:07 +0500

    Add files via upload

---
+++ SpikeCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeCollision : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-80f, 200f));
+            collision.gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+			//Debug.Log("drfr");
+			GameControl.Instance.PlayerDies();
+			//gameControl.playerDies();
+        }
+    }
+}
---
+++ SpikeControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class SpikeControl : MonoBehaviour, IPointerClickHandler
+{
+    public float platformRadius;
+    public GameObject spikes;
+    float maxPos;
+    bool isClicked = false;
+    public float speed;
+    float zPos;
+    void Start()
+    {
+        zPos = transform.position.z;
+    } 
+    void Update()
+    {
+        if (isClicked)
+        {
+            spikes.transform.position = new Vector3(
+                spikes.transform.position.x, 
+                Mathf.Lerp(spikes.transform.position.y, maxPos, Time.deltaTime*speed),
+                zPos);
+      
+        }
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        isClicked = true;
+    }
+    void OnEnable()
+    {
+        maxPos = spikes.transform.position.y - platformRadius;
+    }
+    void OnDisable()
+    {
+		if(isClicked) spikes.transform.position += new Vector3(0, platformRadius, 0);
+		isClicked = false;
+	}
+
+}
