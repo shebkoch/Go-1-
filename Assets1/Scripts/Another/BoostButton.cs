36e8ee6cb131c2705a19bc6a9f7d77e87d12018b
shebkoch <shebkoch@gmail.com>
2017-05-16 23:46:24 +0500

    er

--- Assets1/Scripts/Another/BoostButton.cs
+++ Assets1/Scripts/Another/BoostButton.cs
@@ -1,9 +1,2 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
-public class BoostButton : MonoBehaviour, IPointerClickHandler
-{
-	public float boostSpeed;
-	public void OnPointerClick(PointerEventData eventData) {
-		Time.timeScale = boostSpeed;
-	}
+﻿dc
 }
